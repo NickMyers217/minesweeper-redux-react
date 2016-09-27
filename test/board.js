13b250b4f966944d71d93d7e3d5c1c3ac4d39c41
nickmyers217 <nickbmyers217@gmail.com>
2016-09-26 23:26:57 -0700

    updated the structure of the unit tests

--- test/board.js
+++ test/board.js
@@ -11,11 +11,6 @@ describe('Board', () => {
   const testBoard = emptyBoard(10, 5, () =>
     cell(Map({}), actions.createEmptyCell()))
 
-  it('can create an empty board of a specific width and height', () => {
-    expect(board([], actions.createEmptyBoard(10, 5)))
-      .to.deep.equal(testBoard)
-  })
-
   it('can access and read a cell at a given (x, y) position', () => {
     expect(testBoard.get(3).get(0))
       .to.equal(Map({
@@ -25,110 +20,136 @@ describe('Board', () => {
       }))
   })
 
-  it('can update a cell at a given (x, y) position', () => {
-    const bombAction = actions.updateCell(1, 3, actions.setCellBomb())
-    const bombBoard = board(testBoard, bombAction)
-    expect(bombBoard.get(3).get(1))
-      .to.equal(Map({
-        type: 'BOMB',
-        isFlagged: false,
-        isVisible: false
-      }))
+  describe('#getNeighborCoords()', () => {
+    it('gets the neighbors for a cell in the middle', () => {
+      const middleNeighbors = getNeighborCoords(2, 2, 4, 4)
+      expect(middleNeighbors).to.deep.equal([
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 3, y: 1 },
+        { x: 1, y: 2 },
+        { x: 3, y: 2 },
+        { x: 1, y: 3 },
+        { x: 2, y: 3 },
+        { x: 3, y: 3 }
+      ])
+    })
 
-    const numAction = actions.updateCell(2, 4, actions.setCellNumber(3))
-    const numBoard = board(testBoard, numAction)
-    expect(numBoard.get(4).get(2))
-      .to.equal(Map({
-        type: 'NUMBER',
-        isFlagged: false,
-        isVisible: false,
-        value: 3
-      }))
+    it('gets the neighbors for the top left corner', () => {
+      const topLeftNeighbors = getNeighborCoords(0, 0, 4, 4)
+      expect(topLeftNeighbors).to.deep.equal([
+        { x: 1, y: 0 },
+        { x: 0, y: 1 },
+        { x: 1, y: 1 }
+      ])
+    })
+
+    it('gets the neighbors for the top right corner', () => {
+      const topRightNeighbors = getNeighborCoords(3, 0, 4, 4)
+      expect(topRightNeighbors).to.deep.equal([
+        { x: 2, y: 0 },
+        { x: 2, y: 1 },
+        { x: 3, y: 1 }
+      ])
+    })
+
+    it('gets the neighbors for the bottom left corner', () => {
+      const botLeftNeighbors = getNeighborCoords(0, 3, 4, 4)
+      expect(botLeftNeighbors).to.deep.equal([
+        { x: 0, y: 2 },
+        { x: 1, y: 2 },
+        { x: 1, y: 3 }
+      ])
+    })
+
+    it('gets the neighbors for the bottom right corner', () => {
+      const botRightNeighbors = getNeighborCoords(3, 3, 4, 4)
+      expect(botRightNeighbors).to.deep.equal([
+        { x: 2, y: 2 },
+        { x: 3, y: 2 },
+        { x: 2, y: 3 }
+      ])
+    })
   })
 
-  it('can place a number of bombs in random cells', () => {
-    const countBombs = board => board
-      .flatMap(row => row
-        .filter(cell => cell.get('type') === 'BOMB'))
-          .count()
+  describe('Actions', () => {
+    describe('#createEmptyBoard()', () => {
+      it('can create an empty board of a specific width and height', () => {
+        expect(board([], actions.createEmptyBoard(10, 5)))
+        .to.deep.equal(testBoard)
+      })
+    })
 
-    const bombBoard = board(testBoard, actions.placeBombs(10))
-    expect(countBombs(bombBoard))
-      .to.equal(10)
-  })
+    describe('#updateCell()', () => {
+      it('can update an empty cell as a bomb at a given (x, y) position', () => {
+        const bombAction = actions.updateCell(1, 3, actions.setCellBomb())
+        const bombBoard = board(testBoard, bombAction)
+        expect(bombBoard.get(3).get(1))
+          .to.equal(Map({
+            type: 'BOMB',
+            isFlagged: false,
+            isVisible: false
+          }))
+      })
 
-  it('can retrieve all of a cells neighboring coords on the board', () => {
-    const middleNeighbors = getNeighborCoords(2, 2, 4, 4)
-    expect(middleNeighbors).to.deep.equal([
-      { x: 1, y: 1 },
-      { x: 2, y: 1 },
-      { x: 3, y: 1 },
-      { x: 1, y: 2 },
-      { x: 3, y: 2 },
-      { x: 1, y: 3 },
-      { x: 2, y: 3 },
-      { x: 3, y: 3 }
-    ])
+      it('can update an empty cell as a number at a given (x, y) position', () => {
+        const numAction = actions.updateCell(2, 4, actions.setCellNumber(3))
+        const numBoard = board(testBoard, numAction)
+        expect(numBoard.get(4).get(2))
+          .to.equal(Map({
+            type: 'NUMBER',
+            isFlagged: false,
+            isVisible: false,
+            value: 3
+          }))
+      })
+    })
 
-    const topLeftNeighbors = getNeighborCoords(0, 0, 4, 4)
-    expect(topLeftNeighbors).to.deep.equal([
-      { x: 1, y: 0 },
-      { x: 0, y: 1 },
-      { x: 1, y: 1 }
-    ])
+    describe('#placeBombs()', () => {
+      it('can place a given number of bombs in random cells', () => {
+        const countBombs = board => board
+          .flatMap(row => row
+            .filter(cell => cell.get('type') === 'BOMB'))
+              .count()
+        const bombBoard = board(testBoard, actions.placeBombs(10))
 
-    const topRightNeighbors = getNeighborCoords(3, 0, 4, 4)
-    expect(topRightNeighbors).to.deep.equal([
-      { x: 2, y: 0 },
-      { x: 2, y: 1 },
-      { x: 3, y: 1 }
-    ])
+        expect(countBombs(bombBoard)).to.equal(10)
+      })
+    })
 
-    const botLeftNeighbors = getNeighborCoords(0, 3, 4, 4)
-    expect(botLeftNeighbors).to.deep.equal([
-      { x: 0, y: 2 },
-      { x: 1, y: 2 },
-      { x: 1, y: 3 }
-    ])
+    describe('#calculateNumbers()', () => {
+      it('can calculate all the numbers based on their adjacent bombs', () => {
+        const smallBoard = emptyBoard(4, 4, () =>
+          cell(Map({}), actions.createEmptyCell()))
+        const bombActions = [
+          actions.updateCell(1, 1, actions.setCellBomb()),
+          actions.updateCell(2, 3, actions.setCellBomb()),
+          actions.updateCell(2, 2, actions.setCellBomb())
+        ]
+        const bombBoard = bombActions
+          .reduce((curState, curAction) => board(curState, curAction), smallBoard)
+        const numBoard = board(bombBoard, actions.calculateNumbers())
+        const expectedBoard = fromJS([
+          [ { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
+            { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
+            { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
+            { type: 'EMPTY', isFlagged: false, isVisible: false } ],
+          [ { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
+            { type: 'BOMB', isFlagged: false, isVisible: false },
+            { type: 'NUMBER', value: 2, isFlagged: false, isVisible: false },
+            { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false } ],
+          [ { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
+            { type: 'NUMBER', value: 3, isFlagged: false, isVisible: false },
+            { type: 'BOMB', isFlagged: false, isVisible: false },
+            { type: 'NUMBER', value: 2, isFlagged: false, isVisible: false } ],
+          [ { type: 'EMPTY', isFlagged: false, isVisible: false },
+            { type: 'NUMBER', value: 2, isFlagged: false, isVisible: false },
+            { type: 'BOMB', isFlagged: false, isVisible: false },
+            { type: 'NUMBER', value: 2, isFlagged: false, isVisible: false } ]
+        ])
 
-    const botRightNeighbors = getNeighborCoords(3, 3, 4, 4)
-    expect(botRightNeighbors).to.deep.equal([
-      { x: 2, y: 2 },
-      { x: 3, y: 2 },
-      { x: 2, y: 3 }
-    ])
-  })
-
-  it('can calculate all the numbers based on their adjacent bombs', () => {
-    const smallBoard = emptyBoard(4, 4, () =>
-      cell(Map({}), actions.createEmptyCell()))
-    const bombActions = [
-      actions.updateCell(1, 1, actions.setCellBomb()),
-      actions.updateCell(2, 3, actions.setCellBomb()),
-      actions.updateCell(2, 2, actions.setCellBomb())
-    ]
-    const bombBoard = bombActions
-      .reduce((curState, curAction) => board(curState, curAction), smallBoard)
-    const numBoard = board(bombBoard, actions.calculateNumbers())
-    const expectedBoard = fromJS([
-      [ { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
-        { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
-        { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
-        { type: 'EMPTY', isFlagged: false, isVisible: false } ],
-      [ { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
-        { type: 'BOMB', isFlagged: false, isVisible: false },
-        { type: 'NUMBER', value: 2, isFlagged: false, isVisible: false },
-        { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false } ],
-      [ { type: 'NUMBER', value: 1, isFlagged: false, isVisible: false },
-        { type: 'NUMBER', value: 3, isFlagged: false, isVisible: false },
-        { type: 'BOMB', isFlagged: false, isVisible: false },
-        { type: 'NUMBER', value: 2, isFlagged: false, isVisible: false } ],
-      [ { type: 'EMPTY', isFlagged: false, isVisible: false },
-        { type: 'NUMBER', value: 2, isFlagged: false, isVisible: false },
-        { type: 'BOMB', isFlagged: false, isVisible: false },
-        { type: 'NUMBER', value: 2, isFlagged: false, isVisible: false } ]
-    ])
-
-    expect(numBoard).to.deep.equal(expectedBoard)
+        expect(numBoard).to.deep.equal(expectedBoard)
+      })
+    })
   })
 })
--- test/cell.js
+++ test/cell.js
@@ -21,60 +21,95 @@ describe('Cell', () => {
     .set('isVisible', true)
   const nonVisibleCell = emptyCell
 
-  it('a default empty cell is initialized correctly', () => {
-    expect(cell({}, actions.createEmptyCell()))
-      .to.equal(emptyCell)
-  })
+  describe('Actions', () => {
+    describe('#createEmptyCell()', () => {
+      it('a default empty cell is initialized correctly', () => {
+        expect(cell({}, actions.createEmptyCell()))
+          .to.equal(emptyCell)
+      })
+    })
 
-  it('can turn an empty cell into a bomb. Only an empty cell', () => {
-    expect(cell(emptyCell, actions.setCellBomb()))
-      .to.equal(bombCell)
+    describe('#setCellBomb()', () => {
+      it('can turn an empty cell into a bomb', () => {
+        expect(cell(emptyCell, actions.setCellBomb()))
+          .to.equal(bombCell)
+      })
 
-    expect(cell(bombCell, actions.setCellBomb()))
-      .to.equal(bombCell)
+      it('does not mutate a cell that is already a bomb', () => {
+        expect(cell(bombCell, actions.setCellBomb()))
+          .to.equal(bombCell)
+      })
 
-    expect(cell(numFiveCell, actions.setCellBomb()))
-      .to.equal(numFiveCell)
-  })
+      it('does not mutate a cell that is a number', () => {
+        expect(cell(numFiveCell, actions.setCellBomb()))
+          .to.equal(numFiveCell)
+      })
+    })
 
-  it('can turn an empty cell into a number greater than zero. Only an empty cell', () => {
-    expect(cell(emptyCell, actions.setCellNumber(5)))
-      .to.equal(numFiveCell)
+    describe('#setCellNumber()', () => {
+      it('can turn an empty cell into a number greater than zero', () => {
+        expect(cell(emptyCell, actions.setCellNumber(5)))
+          .to.equal(numFiveCell)
+      })
 
-    expect(cell(bombCell, actions.setCellNumber(5)))
-      .to.equal(bombCell)
+      it('does not mutate the cell if the number is zero or less', () => {
+        expect(cell(emptyCell, actions.setCellNumber(0)))
+          .to.equal(emptyCell)
+      })
 
-    expect(cell(numFiveCell, actions.setCellNumber(5)))
-      .to.equal(numFiveCell)
+      it('does not mutate the cell if it is a bomb', () => {
+        expect(cell(bombCell, actions.setCellNumber(5)))
+          .to.equal(bombCell)
+      })
 
-    expect(cell(emptyCell, actions.setCellNumber(0)))
-      .to.equal(emptyCell)
-  })
+      it('does not mutate the cell if it is a number already', () => {
+        expect(cell(numFiveCell, actions.setCellNumber(6)))
+          .to.equal(numFiveCell)
+      })
+    })
 
-  it('can make a cell visible, but only if its not flagged', () => {
-    expect(cell(emptyCell, actions.setCellVisible()))
-      .to.have.any.keys({'isVisible': true})
+    describe('#setCellVisible()', () => {
+      it('sets an empty cell to visible', () => {
+        expect(cell(emptyCell, actions.setCellVisible()))
+          .to.have.any.keys({'isVisible': true})
+      })
 
-    expect(cell(bombCell, actions.setCellVisible()))
-      .to.have.any.keys({'isVisible': true})
+      it('sets a bomb cell to visible', () => {
+        expect(cell(bombCell, actions.setCellVisible()))
+          .to.have.any.keys({'isVisible': true})
+      })
 
-    expect(cell(numFiveCell, actions.setCellVisible()))
-      .to.have.any.keys({'isVisible': true})
+      it('sets a number cell to visible', () => {
+        expect(cell(numFiveCell, actions.setCellVisible()))
+          .to.have.any.keys({'isVisible': true})
+      })
 
-    expect(cell(emptyCell.set('isFlagged', true), actions.setCellVisible()))
-      .to.equal(emptyCell.set('isFlagged', true))
-  })
+      it('does not mutate a flagged cell', () => {
+        expect(cell(emptyCell.set('isFlagged', true), actions.setCellVisible()))
+          .to.equal(emptyCell.set('isFlagged', true))
+      })
 
-  it('can flag an non visible cell, but not a visible one', () => {
-    expect(cell(nonVisibleCell, actions.setCellFlagged()))
-      .to.equal(nonVisibleCell.set('isFlagged', true))
+      it('does not mutate an already visible cell', () => {
+        expect(cell(emptyCell.set('isVisible', true), actions.setCellVisible()))
+          .to.equal(emptyCell.set('isVisible', true))
+      })
+    })
 
-    expect(cell(visibleCell, actions.setCellFlagged()))
-      .to.equal(visibleCell)
-  })
+    describe('#setCellFlagged()', () => {
+      it('can flag an non visible cell', () => {
+        expect(cell(nonVisibleCell, actions.setCellFlagged()))
+        .to.equal(nonVisibleCell.set('isFlagged', true))
+      })
 
-  it('flagging a flagged cell unflags it', () => {
-    expect(cell(emptyCell.set('isFlagged', true), actions.setCellFlagged()))
-      .to.equal(emptyCell)
+      it('does not mutate a visible cell', () => {
+        expect(cell(visibleCell, actions.setCellFlagged()))
+        .to.equal(visibleCell)
+      })
+
+      it('flagging a flagged cell unflags it', () => {
+        expect(cell(emptyCell.set('isFlagged', true), actions.setCellFlagged()))
+          .to.equal(emptyCell)
+      })
+    })
   })
 })
